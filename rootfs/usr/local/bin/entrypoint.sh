62fac760330401ba2d610fd221a3c5a31607a4a9
casjay <git-admin@casjaysdev.pro>
2026-06-05 18:26:30 -0400

    🐛 Update entrypoint.sh and functions library from current template 🐛
    Stale copies called __initialize_default_templates, __initialize_config_dir,
    and __initialize_data_dir which are not in the old functions library,
    causing container startup failures. Replaced with current template
    versions (202606041210-git) which no longer call those missing functions.
    - rootfs/usr/local/bin/entrypoint.sh: update to current template
    - rootfs/usr/local/etc/docker/functions/entrypoint.sh: update to current template
    
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/etc/docker/functions/entrypoint.sh

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202605260122-git
+##@Version           :  202606041210-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.pro
-# @@License          :  LICENSE.md
+# @@License          :  WTFPL
 # @@ReadME           :  entrypoint.sh --help
 # @@Copyright        :  Copyright: (c) 2026 Jason Hempstead, Casjays Developments
-# @@Created          :  Sunday, May 31, 2026 11:04 EDT
+# @@Created          :  Friday, Jun 05, 2026 18:22 EDT
 # @@File             :  entrypoint.sh
-# @@Description      :  Entrypoint file for rust
+# @@Description      :  Entrypoint file for alpine
 # @@Changelog        :  New script
 # @@TODO             :  Better documentation
 # @@Other            :  
@@ -28,18 +28,18 @@ trap 'retVal=$?;[ "$SERVICE_IS_RUNNING" != "yes" ] && [ -f "$SERVICE_PID_FILE" ]
 [ -f "/config/.debug" ] && [ -z "$DEBUGGER_OPTIONS" ] && export DEBUGGER_OPTIONS="$(<"/config/.debug")" || DEBUGGER_OPTIONS="${DEBUGGER_OPTIONS:-}"
 if [ "$DEBUGGER" = "on" ] || [ -f "/config/.debug" ]; then
   echo "Enabling debugging"
-  set -o pipefail
+  set -eo pipefail
   [ -n "$DEBUGGER_OPTIONS" ] && set -"$DEBUGGER_OPTIONS"
   export DEBUGGER="on"
 else
-  set -o pipefail
+  set -eo pipefail
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 PATH="/usr/local/etc/docker/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 SCRIPT_FILE="$0"
-CONTAINER_NAME="rust"
+CONTAINER_NAME="alpine"
 SCRIPT_NAME="${SCRIPT_FILE##*/}"
 CONTAINER_NAME="${ENV_CONTAINER_NAME:-$CONTAINER_NAME}"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -95,8 +95,8 @@ SERVICE_UID="${SERVICE_UID:-0}"
 SERVICE_GID="${SERVICE_GID:-0}"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # User and group in which the service switches to - IE: nginx,apache,mysql,postgres
-#SERVICE_USER="${SERVICE_USER:-rust}"   # execute command as another user
-#SERVICE_GROUP="${SERVICE_GROUP:-rust}" # Set the service group
+#SERVICE_USER="${SERVICE_USER:-alpine}"   # execute command as another user
+#SERVICE_GROUP="${SERVICE_GROUP:-alpine}" # Set the service group
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Secondary ports
 # specifiy other ports
@@ -155,9 +155,6 @@ export SSL_CA="${SSL_CA:-/config/ssl/ca.crt}"
 export SSL_KEY="${SSL_KEY:-/config/ssl/localhost.pem}"
 export SSL_CERT="${SSL_CERT:-/config/ssl/localhost.crt}"
 export LOCAL_BIN_DIR="${LOCAL_BIN_DIR:-/usr/local/bin}"
-export DEFAULT_DATA_DIR="${DEFAULT_DATA_DIR:-/usr/local/share/template-files/data}"
-export DEFAULT_CONF_DIR="${DEFAULT_CONF_DIR:-/usr/local/share/template-files/config}"
-export DEFAULT_TEMPLATE_DIR="${DEFAULT_TEMPLATE_DIR:-/usr/local/share/template-files/defaults}"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # Backup settings
 export BACKUP_MAX_DAYS="${BACKUP_MAX_DAYS:-}"
@@ -214,15 +211,15 @@ else
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # clean ENV_PORTS variables
-ENV_PORTS="${ENV_PORTS//,/ }"  #
-ENV_PORTS="${ENV_PORTS//\/*/}" #
+ENV_PORTS="${ENV_PORTS//,/ }"
+ENV_PORTS="${ENV_PORTS//\/*/}"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # clean SERVER_PORTS variables
-SERVER_PORTS="${SERVER_PORTS//,/ }"  #
-SERVER_PORTS="${SERVER_PORTS//\/*/}" #
+SERVER_PORTS="${SERVER_PORTS//,/ }"
+SERVER_PORTS="${SERVER_PORTS//\/*/}"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # clean WEB_SERVER_PORTS variables
-WEB_SERVER_PORTS="${WEB_SERVER_PORT//,/ } ${ENV_WEB_SERVER_PORTS//,/ }" #
+WEB_SERVER_PORTS="${WEB_SERVER_PORT//,/ } ${ENV_WEB_SERVER_PORTS//,/ }"
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # rewrite and merge variables
 ENV_PORTS="$(__format_variables "$ENV_PORTS" || false)"
@@ -294,7 +291,7 @@ fi
 if [ "$ENTRYPOINT_FIRST_RUN" != "no" ]; then
   if [ "$CONFIG_DIR_INITIALIZED" = "no" ] || [ "$DATA_DIR_INITIALIZED" = "no" ]; then
     if [ "$ENTRYPOINT_MESSAGE" = "yes" ]; then
-      echo "Executing entrypoint script for rust"
+      echo "Executing entrypoint script for alpine"
     fi
   fi
   # - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -407,21 +404,6 @@ if [ "$ENTRYPOINT_FIRST_RUN" != "no" ] || [ "$CONFIG_DIR_INITIALIZED" = "no" ] |
   # Setup bin directory - /config/bin > /usr/local/bin
   __initialize_custom_bin_dir
   # - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Copy default system configs - /usr/local/share/template-files/defaults > /config/
-  if [ "$CONFIG_DIR_INITIALIZED" = "no" ]; then
-    __initialize_default_templates
-  fi
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Copy custom config files - /usr/local/share/template-files/config > /config/
-  if [ "$CONFIG_DIR_INITIALIZED" = "no" ]; then
-    __initialize_config_dir
-  fi
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Copy custom data files - /usr/local/share/template-files/data > /data/
-  if [ "$DATA_DIR_INITIALIZED" = "no" ]; then
-    __initialize_data_dir
-  fi
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
   # Initialize SSL certificates
   __initialize_ssl_certs
   # - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -506,11 +488,9 @@ if [ "$START_SERVICES" = "yes" ] || [ -z "$1" ]; then
     echo "$$" >"$ENTRYPOINT_PID_FILE"
     __start_init_scripts "/usr/local/etc/docker/init.d"
     CONTAINER_INIT="${CONTAINER_INIT:-no}"
-    # Only block in monitoring mode when no user command was given
-    if [ $# -eq 0 ]; then
-      __no_exit
-      exit $?
-    fi
+    # Services started successfully - enter monitoring mode
+    __no_exit
+    exit $?
   fi
   START_SERVICES="no"
 fi
@@ -676,8 +656,11 @@ start)
 # Execute primary command
 *)
   if [ $# -eq 0 ]; then
-    # No args: run the default Rust workflow (fmt → clippy → test → build)
-    __exec_command rust-workflow
+    if [ ! -f "$ENTRYPOINT_PID_FILE" ]; then
+      echo "$$" >"$ENTRYPOINT_PID_FILE"
+      [ "$START_SERVICES" = "no" ] && [ "$CONTAINER_INIT" = "yes" ] || __start_init_scripts "/usr/local/etc/docker/init.d"
+    fi
+    __no_exit
   else
     __exec_command "$@"
   fi
